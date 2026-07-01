1ca485b165476262223fa64afb8e1fd5d0cc0a75
みらい <s2411870@u.tsukuba.ac.jp>
2026-07-01 13:51:03 +0900

    準備中ページ基盤作成

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Class from "./pages/Class";
+import ComingSoonPage from "./pages/ComingSoon";
 import Notfound404 from "./pages/404";
 import Confirm from "./components/auth/Confirm";
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/class" element={<Class />} />
         <Route path="/auth/confirm" element={<Confirm />} />
+        <Route path="/coming-soon" element={<ComingSoonPage />} />
 
         {/* どのルートにもマッチしなかった場合は404ページを表示 */}
         <Route path="*" element={<Notfound404 />} />
---
+++ src/pages/ComingSoon.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/utility/Footer";
+
+function ComingSoonPage() {
+  return (
+    <>
+      <div>準備中</div>
+      <Footer />
+    </>
+  );
+}
+
+export default ComingSoonPage;
